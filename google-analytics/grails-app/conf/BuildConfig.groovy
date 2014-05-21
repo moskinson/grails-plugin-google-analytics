36492fc44c9bd1cc7ff0c82ccfd07525f2d520d4
Javier Moscardo <jmoscardo@unience.com>
2014-05-21 12:44:57 +0200

    Change to Spock framework for Testing

--- google-analytics/grails-app/conf/BuildConfig.groovy
+++ google-analytics/grails-app/conf/BuildConfig.groovy
@@ -20,13 +20,18 @@ grails.project.dependency.resolution = {
 
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
+        test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
     }
 
     plugins {
-       build ':release:2.2.1', ':rest-client-builder:1.0.3', {
-          export = false
-       }
+       
+        build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+            export = false
+        }
+
+        test(":spock:0.7") {
+            exclude "spock-grails-support"
+        }
     }
 
 }
